ef11da16e2cc5dd2932ef50353834f4841ea54ef
Zoltan Szinegh <zoltan.szinegh@gmail.com>
2014-04-10 17:04:44 -0400

    Add rounding to input and output

--- pyconvert/main.py
+++ pyconvert/main.py
@@ -1,4 +1,11 @@
-"""Main program module."""
+"""Main program module.
+
+A front end for the unit conversions in 'conversions.py'.
+
+This module can be imported like 'import pyconvert.main' and
+run by calling 'pyconvert.main.main()'.
+
+"""
 
 import sys
 import subprocess
@@ -6,8 +13,6 @@ from collections import OrderedDict
 
 import pyconvert.conversions as convert
 
-
-CLEAR_CMD = 'clear'
 
 MENU_DICT = OrderedDict(
     [
@@ -20,16 +25,32 @@ MENU_DICT = OrderedDict(
 )
 
 
+def get_running_os():
+    """Get the operating system."""
+
+    # Check for windoze to get clear screen command.
+    if sys.platform.startswith('win'):
+        return 'cls'
+    else:
+        return 'clear'
+
+
 def clr_screen(clear_cmd):
-    """Clear console screen."""
+    """Clear console screen.
+
+    Args:
+        clear_cmd (str): Platform specific clear screen command.
+    """
     subprocess.call(clear_cmd)
 
 
 def main():
-    """Main function."""
+    """Run the program in the console."""
+
+    clear_cmd = get_running_os()
 
     while True:
-        clr_screen(CLEAR_CMD)
+        clr_screen(clear_cmd)
 
         print "\t Main Menu"
         print "---------------------------"
@@ -42,12 +63,14 @@ def main():
         try:
             selection = int(raw_input())
         except ValueError:
+            print
             print 'Please enter a valid number.'
-            raw_input('Press ENTER to continue.')
+            raw_input('Press ENTER to continue...')
         else:
             if selection not in MENU_DICT:
+                print
                 print 'Invalid menu selection.'
-                raw_input('Press ENTER to continue.')
+                raw_input('Press ENTER to continue...')
             elif selection == 5:
                 print
                 print 'Quitting...'
@@ -60,16 +83,23 @@ def main():
                 num_to_convert = convert.validate_input(num_to_convert)
 
                 if not isinstance(num_to_convert, float):
+                    print
                     print '%s is not a number.' % num_to_convert
-                    raw_input('Press ENTER to continue.')
+                    raw_input('Press ENTER to continue...')
                 else:
                     result = MENU_DICT[selection][1](num_to_convert)
+
+                    # Round input and output to 1 decimal place.
+                    num_to_convert = round(num_to_convert, 1)
+                    result = round(result, 1)
+
                     unit_from = MENU_DICT[selection][0].split()[0]
                     unit_to = MENU_DICT[selection][0].split()[2]
 
-                    print('%f in %s is %f in %s' %
+                    print
+                    print('%.1f in %s is %.1f in %s.' %
                           (num_to_convert, unit_from, result, unit_to))
-                    raw_input('Press ENTER to continue.')
+                    raw_input('Press ENTER to continue...')
 
     sys.exit(0)
 
